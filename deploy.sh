5f1ef30149e8decdb9059875062434da7f9d0741
adrian <alai22@gmail.com>
2025-10-22 23:36:17 -0700

    Fix missing AWS credentials in Docker production deployment
    - Add AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY to production docker run command
    - Fixes 'Unable to locate credentials' error when accessing S3
    - Allows conversation tracker to properly save/load data from S3

--- deploy.sh
+++ deploy.sh
@@ -94,6 +94,8 @@ case $ENVIRONMENT in
             -e S3_BUCKET_NAME="${S3_BUCKET_NAME}" \
             -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" \
             -e AWS_REGION="${AWS_DEFAULT_REGION}" \
+            -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+            -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
             --name gladly-prod \
             $PROJECT_NAME:$ENVIRONMENT
         
